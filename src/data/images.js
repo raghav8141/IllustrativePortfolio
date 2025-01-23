bb50abc3bdc6c19e64bc4b1d3c307d814c2ae25a
YASHVI <yashvi.lad@novascotia.ca>
2025-01-23 11:53:46 -0400

    Changes to slider

--- src/data/images.js
+++ src/data/images.js
@@ -1,228 +1,10 @@
 import '../data/custom-image-gallery.css';
 
 export const images = [
-  {
-    id: 1,
-	 original: require('../images/DSC_0890.JPG'),
-    thumbnail: require('../images/DSC_0890.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 2,
-	 original: require('../images/DSC_0932.JPG'),
-    thumbnail: require('../images/DSC_0932.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 3,
-	 original: require('../images/DSC_0927.JPG'),
-    thumbnail: require('../images/DSC_0927.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 4,
-	 original: require('../images/DSC_0799.JPG'),
-    thumbnail: require('../images/DSC_0799.JPG'),
-	 loading: "lazy",
-  },
-  {
-	 id: 5,
-    original: require('../images/DSC_0899.JPG'),
-    thumbnail: require('../images/DSC_0899.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 6,
-	 original: require('../images/DSC_0907.JPG'),
-    thumbnail: require('../images/DSC_0907.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 7,
-	 original: require('../images/DSC_0898.JPG'),
-    thumbnail: require('../images/DSC_0898.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 8,
-	 original: require('../images/CSC_0904.JPG'),
-    thumbnail: require('../images/CSC_0904.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 9,
-	 original: require('../images/DSC_0901.JPG'),
-    thumbnail: require('../images/DSC_0901.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 10,
-	 original: require('../images/DSC_1070.JPG'),
-    thumbnail: require('../images/DSC_1070.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 11,
-	 original: require('../images/IMG_E0421.JPG'),
-    thumbnail: require('../images/IMG_E0421.JPG'),
-	 loading: "lazy",
-  },
-  
-  {
-    id: 12,
-	 original: require('../images/DSC_0717.JPG'),
-    thumbnail: require('../images/DSC_0717.JPG'),
-	 loading: "lazy",
-  },
-  
-  {
-    id: 13,
-	 original: require('../images/IMG_0318.JPG'),
-    thumbnail: require('../images/IMG_0318.JPG'),
-	 loading: "lazy",
-  },
-  
-	{
-		id: 14,
-		original: require('../images/IMG_E0412.JPG'),
-		thumbnail: require('../images/IMG_E0412.JPG'),
-		loading: "lazy",
-	},
-  
-
-  {
-    id: 15,
-	 original: require('../images/IMG_0473.JPG'),
-    thumbnail: require('../images/IMG_0473.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 16,
-	 original: require('../images/DSC_0889.JPG'),
-    thumbnail: require('../images/DSC_0889.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 17,
-	 original: require('../images/IMG_2276.JPG'),
-    thumbnail: require('../images/IMG_2276.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 18,
-	 original: require('../images/DSC_1087.JPG'),
-    thumbnail: require('../images/DSC_1087.JPG'),
-	 loading: "lazy",
-  },
-
-  {
-    id: 19,
-	 original: require('../images/DSC_0879.JPG'),
-    thumbnail: require('../images/DSC_0879.JPG'),
-	 loading: "lazy",
-  },
-
-  {
-    id: 20,
-	 original: require('../images/IMG_E0446.JPG'),
-    thumbnail: require('../images/IMG_E0446.JPG'),
-	 loading: "lazy",
-  },
-  
-  {
-    id: 21,
-	 original: require('../images/IMG_7430.JPG'),
-    thumbnail: require('../images/IMG_7430.JPG'),
-	 loading: "lazy"
-  },
-
-
-  {
-    id: 22,
-	 original: require('../images/DSC_1088.JPG'),
-    thumbnail: require('../images/DSC_1088.JPG'),
-	 loading: "lazy",
-  },
-
-  {
-    id: 23,
-	 original: require('../images/DSC_0934.JPG'),
-    thumbnail: require('../images/DSC_0934.JPG'),
-	 loading: "lazy",
-  },
-  
-
-  {
-    id: 24,
-	 original: require('../images/IMG_2460.JPG'),
-    thumbnail: require('../images/IMG_2460.JPG'),
-	 loading: "lazy",
-  },
-
-  {
-    id: 25,
-	 original: require('../images/DSC_1045.JPG'),
-    thumbnail: require('../images/DSC_1045.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 26,
-	 original: require('../images/DSC_1033.JPG'),
-    thumbnail: require('../images/DSC_1033.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 27,
-	 original: require('../images/IMG_5099.JPG'),
-    thumbnail: require('../images/IMG_5099.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 28,
-	 original: require('../images/DSC_1093.JPG'),
-    thumbnail: require('../images/DSC_1093.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 29,
-	 original: require('../images/IMG_3320.jpg'),
-    thumbnail: require('../images/IMG_3320.jpg'),
-	 loading: "lazy",
-  },
-  {
-    id: 30,
-	 original: require('../images/IMG_3236.jpg'),
-    thumbnail: require('../images/IMG_3236.jpg'),
-	 loading: "lazy",
-  },
-  {
-    id: 31,
-	 original: require('../images/IMG_5227.JPG'),
-    thumbnail: require('../images/IMG_5227.JPG'),
-	 loading: "lazy",
-  },
-   {
-    id: 32,
-	 original: require('../images/IMG_5221.jpg'),
-    thumbnail: require('../images/IMG_5221.jpg'),
-	 loading: "lazy",
-  },
-  {
-    id: 33,
-	 original: require('../images/IMG_5214.JPG'),
-    thumbnail: require('../images/IMG_5214.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 34,
-	 original: require('../images/IMG_5223.JPG'),
-    thumbnail: require('../images/IMG_5223.JPG'),
-	 loading: "lazy",
-  },
-  {
-    id: 35,
-	 original: require('../images/IMG_5213.JPG'),
-    thumbnail: require('../images/IMG_5213.JPG'),
+	 {
+    id: 37,
+	 original: require('../images/IMG_6650.jpeg'),
+    thumbnail: require('../images/IMG_6650.jpeg'),
 	 loading: "lazy",
   },
   {
@@ -232,12 +14,215 @@ export const images = [
 	 loading: "lazy",
   },
   {
-    id: 37,
-	 original: require('../images/IMG_6650.jpeg'),
-    thumbnail: require('../images/IMG_6650.jpeg'),
+    id: 35,
+	 original: require('../images/IMG_5213.JPG'),
+    thumbnail: require('../images/IMG_5213.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 34,
+	 original: require('../images/IMG_5223.JPG'),
+    thumbnail: require('../images/IMG_5223.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 33,
+	 original: require('../images/IMG_5214.JPG'),
+    thumbnail: require('../images/IMG_5214.JPG'),
+	 loading: "lazy",
+  },
+   {
+    id: 32,
+	 original: require('../images/IMG_5221.jpg'),
+    thumbnail: require('../images/IMG_5221.jpg'),
+	 loading: "lazy",
+  },
+  {
+    id: 31,
+	 original: require('../images/IMG_5227.JPG'),
+    thumbnail: require('../images/IMG_5227.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 30,
+	 original: require('../images/IMG_3236.jpg'),
+    thumbnail: require('../images/IMG_3236.jpg'),
+	 loading: "lazy",
+  },
+  {
+    id: 29,
+	 original: require('../images/IMG_3320.jpg'),
+    thumbnail: require('../images/IMG_3320.jpg'),
+	 loading: "lazy",
+  },
+  {
+    id: 28,
+	 original: require('../images/DSC_1093.JPG'),
+    thumbnail: require('../images/DSC_1093.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 27,
+	 original: require('../images/IMG_5099.JPG'),
+    thumbnail: require('../images/IMG_5099.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 26,
+	 original: require('../images/DSC_1033.JPG'),
+    thumbnail: require('../images/DSC_1033.JPG'),
+	 loading: "lazy",
+  },
+   {
+    id: 25,
+	 original: require('../images/DSC_1045.JPG'),
+    thumbnail: require('../images/DSC_1045.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 24,
+	 original: require('../images/IMG_2460.JPG'),
+    thumbnail: require('../images/IMG_2460.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 23,
+	 original: require('../images/DSC_0934.JPG'),
+    thumbnail: require('../images/DSC_0934.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 22,
+	 original: require('../images/DSC_1088.JPG'),
+    thumbnail: require('../images/DSC_1088.JPG'),
+	 loading: "lazy",
+  },
+   {
+    id: 21,
+	 original: require('../images/IMG_7430.JPG'),
+    thumbnail: require('../images/IMG_7430.JPG'),
+	 loading: "lazy"
+  },
+  {
+    id: 20,
+	 original: require('../images/IMG_E0446.JPG'),
+    thumbnail: require('../images/IMG_E0446.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 19,
+	 original: require('../images/DSC_0879.JPG'),
+    thumbnail: require('../images/DSC_0879.JPG'),
+	 loading: "lazy",
+  },
+   {
+    id: 18,
+	 original: require('../images/DSC_1087.JPG'),
+    thumbnail: require('../images/DSC_1087.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 17,
+	 original: require('../images/IMG_2276.JPG'),
+    thumbnail: require('../images/IMG_2276.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 16,
+	 original: require('../images/DSC_0889.JPG'),
+    thumbnail: require('../images/DSC_0889.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 15,
+	 original: require('../images/IMG_0473.JPG'),
+    thumbnail: require('../images/IMG_0473.JPG'),
+	 loading: "lazy",
+  },
+  {
+		id: 14,
+		original: require('../images/IMG_E0412.JPG'),
+		thumbnail: require('../images/IMG_E0412.JPG'),
+		loading: "lazy",
+	},
+	{
+    id: 13,
+	 original: require('../images/IMG_0318.JPG'),
+    thumbnail: require('../images/IMG_0318.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 12,
+	 original: require('../images/DSC_0717.JPG'),
+    thumbnail: require('../images/DSC_0717.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 11,
+	 original: require('../images/IMG_E0421.JPG'),
+    thumbnail: require('../images/IMG_E0421.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 10,
+	 original: require('../images/DSC_1070.JPG'),
+    thumbnail: require('../images/DSC_1070.JPG'),
+	 loading: "lazy",
+  },
+   {
+    id: 9,
+	 original: require('../images/DSC_0901.JPG'),
+    thumbnail: require('../images/DSC_0901.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 8,
+	 original: require('../images/CSC_0904.JPG'),
+    thumbnail: require('../images/CSC_0904.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 7,
+	 original: require('../images/DSC_0898.JPG'),
+    thumbnail: require('../images/DSC_0898.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 6,
+	 original: require('../images/DSC_0907.JPG'),
+    thumbnail: require('../images/DSC_0907.JPG'),
+	 loading: "lazy",
+  },
+  {
+	 id: 5,
+    original: require('../images/DSC_0899.JPG'),
+    thumbnail: require('../images/DSC_0899.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 4,
+	 original: require('../images/DSC_0799.JPG'),
+    thumbnail: require('../images/DSC_0799.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 3,
+	 original: require('../images/DSC_0927.JPG'),
+    thumbnail: require('../images/DSC_0927.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 2,
+	 original: require('../images/DSC_0932.JPG'),
+    thumbnail: require('../images/DSC_0932.JPG'),
+	 loading: "lazy",
+  },
+  {
+    id: 1,
+	 original: require('../images/DSC_0890.JPG'),
+    thumbnail: require('../images/DSC_0890.JPG'),
 	 loading: "lazy",
   }
-  
 ];
 
 
